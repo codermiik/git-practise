8e8afc0f688c5016883d15bc312779aa6240d87d
glent <williamsumba2019@gmail.com>
2023-02-05 19:12:29 +0300

    first release

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get references to elements on the page
+const startDetectionBtn = document.getElementById("start-detection-btn");
+const takePhotoBtn = document.getElementById("take-photo-btn");
+const uploadPhotoBtn = document.getElementById("upload-photo-btn");
+const tryAgainBtn = document.getElementById("try-again-btn");
+const saveResultsBtn = document.getElementById("save-results-btn");
+const homeScreen = document.querySelect

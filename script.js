c00d7fbadc2e8f0efb7cfa6327cd06d7fa0b26a9
glent <williamsumba2019@gmail.com>
2023-02-17 06:25:14 +0300

    updates

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// Get references to elements on the page
+/*Get references to elements on the page
 const startDetectionBtn = document.getElementById("start-detection-btn");
 const takePhotoBtn = document.getElementById("take-photo-btn");
 const uploadPhotoBtn = document.getElementById("upload-photo-btn");
 const tryAgainBtn = document.getElementById("try-again-btn");
 const saveResultsBtn = document.getElementById("save-results-btn");
-const homeScreen = document.querySelect
+const homeScreen = document.querySelect*/
+
+console.log("hello world")
+ let a=2
+ let b=5
+ 
+ console.log(a+b)
+
+ const cars=["volvo","mercedes","range rover","porche"]
+ cars[2]="cadillac"
+ cars[4]="toyota"
+ //document.getElementById("demo").innerHTML=cars
+ cars.unshift("cayan")
+ console.log(cars)
+ 
+ 
